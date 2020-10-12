adf02724f9aa01b8dccde6f18c8d4c7e7605aee1
paulorojas <paulorojas@gmail.com>
2020-10-11 22:42:42 -0600

    Tercer ciclo de cambios

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tu+cx6d6db0-*sujg^zl3yio(q++!*zq55ww@5qxt18&in)qaf#$'
+SECRET_KEY = 'PAULOtu+cx6d6db0-*sujg^zl3yio(q++!*zq55ww@5qxt18&in)qaf#$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
